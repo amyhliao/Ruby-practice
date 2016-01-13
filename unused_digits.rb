25714046a9a850794e4c60b614c15c1383607a25
Amy Liao <amyhliao88@gmail.com>
2016-01-12 22:36:53 -0800

    add directions to ruby challenge

--- unused_digits.rb
+++ unused_digits.rb
@@ -1,1 +1,12 @@
-unused_digits.rb
+# Directions:
+
+# Given few numbers, you need to print out the digits that are not being used.
+
+# Example:
+
+# unused_digits(12, 34, 56, 78) # "09"
+# unused_digits(2015, 8, 26) # "3479"
+# Note:
+
+# Result string should be sorted
+# The test case won't pass Integer with leading zero
